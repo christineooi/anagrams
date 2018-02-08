73f69eecbcd0c1b8bd8b7d0dee84b6d94994b6f2
Christine Ooi <christine.ooi@gmail.com>
2018-02-08 11:59:10 -0500

    Modified html, js, and added words.js

---
+++ anagrams.js
@@ -0,0 +1,10 @@
+
+var typedText = document.getElementById("input").value;
+
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
+
+document.getElementById("findButton").onclick = function() {
+    
+ }
